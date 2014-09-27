5d5acc3801884ee0919c312e07351b93043fd610
paolo <liwanag.paolo@gmail.com>
2014-09-27 12:21:17 -0700

    re-Commit
    Accidentally deleted my repo :(

---
+++ js/java.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+var enterNumber = prompt("Enter a Number");
+var convertNumber = +enterNumber;
+var fizzBuzzFunc = function(){
+	for(var i = 1; i < +convertNumber +1; i++){
+		if((i % 3 === 0) && (i % 5 === 0)){
+	document.write("Fizz Buzz ");
+	}
+		else if(i % 3 === 0){
+	document.write("Fizz ");
+	}
+		else if(i % 5 === 0){
+	document.write("Buzz ");
+	}
+		else{
+	document.write(" " + i + " ");
+	}
+}
+};
+//check for an illegal number
+if(isNaN(convertNumber)){
+	prompt("Please enter a number only!");
+}
+else if(convertNumber % 1 !== 0)
+{
+	prompt("Please enter a whole number only!");
+}
+else{
+	fizzBuzzFunc();
+}
+
+$("#numbers").append(fizzBuzzFunc());
+});
